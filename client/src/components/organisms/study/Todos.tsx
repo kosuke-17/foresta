11af4f6c42eb5a34d7bdf1418352c5e81ff38b0f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-28 18:31:16 +0900

    Todoリストタブの大枠完成

--- client/src/components/organisms/study/Todos.tsx
+++ client/src/components/organisms/study/Todos.tsx
@@ -1,8 +1,32 @@
 import { FC, memo } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 export const Todos: FC = memo(() => {
+  return (
+    <>
+      <h2>Todoリスト</h2>
+      <Tabs variant="soft-rounded" colorScheme='green' isLazy>
+        <TabList>
+          <Tab>全て</Tab>
+          <Tab>今日</Tab>
+          <Tab>期限切れ</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>全てのTodo</p>
   return (<>
     Todos
     
   </>);
+          </TabPanel>
+          <TabPanel>
+            <p>今日のTodo</p>
+          </TabPanel>
+          <TabPanel>
+            <p>期限切れ</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 });
