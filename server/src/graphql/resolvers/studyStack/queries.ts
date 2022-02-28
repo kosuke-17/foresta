f100ec240cc16a2a091ac1fd51269c1a0c6ea184
kosuke-17 <tamutamu17@icloud.com>
2022-02-28 22:02:21 +0900

    学習記録IDに紐づいた学習記録を取得する

--- server/src/graphql/resolvers/studyStack/queries.ts
+++ server/src/graphql/resolvers/studyStack/queries.ts
@@ -7,8 +7,17 @@ const studyStackQueries = {
    * @param userId - userID
    * @returns StudyStack一覧情報
    */
-  getAllStudyStackByUserId: async (_parent: any, { userId }: any) => {
+  getAllStudyStackByUserId: async (_: any, { userId }: any) => {
     return await StudyStack.find({ userId: userId });
+  },
+  /**
+   * 学習記録IDに紐づく学習記録の取得
+   *
+   * @param studyStackId - 学習記録ID
+   * @returns 学習記録IDに紐づく学習記録
+   */
+  getStudyStackByStudyStackId: async (_: any, { studyStackId }: any) => {
+    return await StudyStack.findById({ _id: studyStackId });
   },
 };
 
