3a2854d0ab83bc122d6aeb4e6d85c48d21a63988
kosuke-17 <tamutamu17@icloud.com>
2022-02-28 21:55:10 +0900

    StudyStack一覧情報を取得

--- server/src/graphql/resolvers/studyStack/queries.ts
+++ server/src/graphql/resolvers/studyStack/queries.ts
@@ -1,3 +1,15 @@
-const studyStackQueries = {};
+import { StudyStack } from "../../../models/StudyStack.model";
+
+const studyStackQueries = {
+  /**
+   * ユーザーのStudyStack一覧情報を取得する.
+   *
+   * @param userId - userID
+   * @returns StudyStack一覧情報
+   */
+  getAllStudyStackByUserId: async (_parent: any, { userId }: any) => {
+    return await StudyStack.find({ userId: userId });
+  },
+};
 
 export default studyStackQueries;
