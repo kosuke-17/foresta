f260c1d28b1be5b70f76af84eaf76617f089b473
kosuke-17 <tamutamu17@icloud.com>
2022-02-28 21:20:07 +0900

    学習記録を削除する処理実装

--- server/src/graphql/resolvers/studyStack/mutations.ts
+++ server/src/graphql/resolvers/studyStack/mutations.ts
@@ -2,9 +2,15 @@ import { StudyStack } from "../../../models/StudyStack.model";
 import { success } from "../responseStatus";
 
 const studyStackMutations = {
+  /**
+   * 学習記録を追加する.
+   *
+   * @param stack - 学習記録情報
+   * @returns successステータス, 作成した学習記録の情報
+   * @returns errorステータス
+   */
   addStudyStack: (_: any, { stack }: any) => {
     const { content, timeStack, createdAt, userId } = stack;
-
     try {
       const newStudyStack = new StudyStack({
         content,
@@ -14,6 +20,21 @@ const studyStackMutations = {
       });
       const result = newStudyStack.save();
       return success(result);
+    } catch (error) {
+      // 必須のデータがnullだとエラーを返す
+      return { status: "error" };
+    }
+  },
+  /**
+   * 学習記録を削除する.
+   *
+   * @param studyStackId - 学習記録ID
+   * @returns successステータス or errorステータス
+   */
+  removeStudyStack: async (_: any, { studyStackId }: any) => {
+    try {
+      await StudyStack.deleteOne({ _id: studyStackId });
+      return { status: "success" };
     } catch (error) {
       // 必須のデータがnullだとエラーを返す
       return { status: "error" };
