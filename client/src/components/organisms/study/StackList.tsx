c96054f6f46b8fb9cebd766c4a438c2b0f70339c
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-03-01 10:27:46 +0900

    更新情報テーブル作成

--- client/src/components/organisms/study/StackList.tsx
+++ client/src/components/organisms/study/StackList.tsx
@@ -60,7 +60,41 @@ export const StackList = () => {
             </Table>
           </TabPanel>
           <TabPanel>
-            
+            <Table variant="striped" colorScheme="green">
+              <TableCaption>更新情報</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>記録日時</Th>
+                  <Th>技術内容</Th>
+                  <Th>学習時間</Th>
+                  <Th></Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>2022/2/2</Td>
+                  <Td>React</Td>
+                  <Td>1時間</Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td>2022/1/2</Td>
+                  <Td>React</Td>
+                  <Td>40分</Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td>2022/2/20</Td>
+                  <Td>Vue</Td>
+                  <Td>1時間</Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+              </Tbody>
+            </Table>
           </TabPanel>
         </TabPanels>
       </Tabs>
