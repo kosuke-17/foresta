d4c9b9d91aceaa4aaa4b8ea720faa6051361470c
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-01 15:59:05 +0900

    アコーディオン導入(ボタンの青フチが消せない…)

--- client/src/container/templates/AboutMe.tsx
+++ client/src/container/templates/AboutMe.tsx
@@ -11,6 +11,11 @@ import {
   Tr,
   Th,
   Td,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
 } from "@chakra-ui/react";
 
 export const AboutMe: FC = memo(() => {
@@ -41,51 +46,62 @@ export const AboutMe: FC = memo(() => {
         </div>
       </Box>
       <Box background={"white.50"} m={10} p={20} rounded={20}>
-        <_Title>基本情報</_Title>
-        <Table variant="striped" colorScheme="gray">
-          <Thead>
-            <Tr>
-              <Th>項目名</Th>
-              <Th>内容</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>スタッフID</Td>
-              <Td>0000</Td>
-            </Tr>
-            <Tr>
-              <Td>年齢</Td>
-              <Td>00歳</Td>
-            </Tr>
-            <Tr>
-              <Td>性別</Td>
-              <Td>女</Td>
-            </Tr>
-            <Tr>
-              <Td>最寄駅</Td>
-              <Td>ほげほげ駅(ほげほげ線)</Td>
-            </Tr>
-            <Tr>
-              <Td>稼働開始日</Td>
-              <Td>応相談</Td>
-            </Tr>
-            <Tr>
-              <Td>エンジニア歴</Td>
-              <Td>
-                0年0ヵ月
-                <ul>
-                  <li>SE経験:0年0ヵ月</li>
-                  <li>PG/作業員経験:0年0ヵ月</li>
-                </ul>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>IT全体歴</Td>
-              <Td>0年0ヵ月</Td>
-            </Tr>
-          </Tbody>
-        </Table>
+        <Accordion allowMultiple>
+          <AccordionItem outlineOffset={0}>
+            <AccordionButton>
+              <Box flex="1" textAlign="left" fontWeight="bold" fontSize={30}>
+                基本情報
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+              <Table variant="striped" colorScheme="gray">
+                <Thead>
+                  <Tr>
+                    <Th>項目名</Th>
+                    <Th>内容</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>スタッフID</Td>
+                    <Td>0000</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>年齢</Td>
+                    <Td>00歳</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>性別</Td>
+                    <Td>女</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>最寄駅</Td>
+                    <Td>ほげほげ駅(ほげほげ線)</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>稼働開始日</Td>
+                    <Td>応相談</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>エンジニア歴</Td>
+                    <Td>
+                      0年0ヵ月
+                      <ul>
+                        <li>SE経験:0年0ヵ月</li>
+                        <li>PG/作業員経験:0年0ヵ月</li>
+                      </ul>
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td>IT全体歴</Td>
+                    <Td>0年0ヵ月</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
 
         <_Title>スキル要約</_Title>
 
