620678aa46c241386028cb76d21fbcf9c1b9f55c
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-01 14:28:06 +0900

    カレンダー用コンポーネント作成

---
+++ client/src/components/molucules/calendar.tsx
@@ -0,0 +1,10 @@
+import { FC, memo } from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+export const Calendar: FC = memo(() => {
+  return (
+
+  );
+});
