2a56122a44c92b5ed26c6dca78a39df2597af387
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-03-01 15:51:18 +0900

    仮のユーザーIDからデータを取得してmapで表示（keyをindexにしているが本当はIDがよい）

--- client/src/components/molucules/stackList/LogListTable.tsx
+++ client/src/components/molucules/stackList/LogListTable.tsx
@@ -8,71 +8,61 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { memo } from "react";
+import { useGetAllStudyStackByUserIdQuery } from "../../../types/generated/graphql";
 
 /**
  * 更新情報を表示するテーブル
  * @returns 更新情報テーブル
  */
-export const LogListTable = () => {
-  return (
-    <Table variant="striped" colorScheme="green">
-      <TableCaption>更新情報</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>記録日時</Th>
-          <Th>技術内容</Th>
-          <Th>学習時間</Th>
-          <Th></Th>
-          <Th></Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        <Tr>
-          <Td>2022/2/2</Td>
-          <Td>React</Td>
-          <Td>1時間</Td>
-          <Td>
-            <Button colorScheme="green" size="sm">
-              編集
-            </Button>
-          </Td>
-          <Td>
-            <Button colorScheme="green" variant="outline" size="sm">
-              削除
-            </Button>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>2022/1/2</Td>
-          <Td>React</Td>
-          <Td>40分</Td>
-          <Td>
-            <Button colorScheme="green" size="sm">
-              編集
-            </Button>
-          </Td>
-          <Td>
-            <Button colorScheme="green" variant="outline" size="sm">
-              削除
-            </Button>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>2022/2/20</Td>
-          <Td>Vue</Td>
-          <Td>1時間</Td>
-          <Td>
-            <Button colorScheme="green" size="sm">
-              編集
-            </Button>
-          </Td>
-          <Td>
-            <Button colorScheme="green" variant="outline" size="sm">
-              削除
-            </Button>
-          </Td>
-        </Tr>
-      </Tbody>
-    </Table>
-  );
-};
+export const LogListTable = memo(() => {
+  //userIdから学習記録データを取得する
+  const { loading, data, error } = useGetAllStudyStackByUserIdQuery({
+    //仮のユーザーID
+    variables: { userId: "621c795fea18ffdb80e66462" },
+  });
+  console.log(data);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  } else if (error) {
+    return <p>Error!</p>;
+  } else {
+    return (
+      <Table variant="striped" colorScheme="green">
+        <TableCaption>更新情報</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>記録日時</Th>
+            <Th>技術内容</Th>
+            <Th>学習時間</Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {data &&
+            data.getAllStudyStackByUserId.map((stackList, index) => (
+              //キーをindexにしているが、IDにしたい
+              <Tr key={index}>
+                <Td>{stackList.createdAt}</Td>
+                <Td>{stackList.skillTagId}</Td>
+                <Td>{stackList.timeStack}分</Td>
+                <Td>
+                  <Button colorScheme="green" size="sm">
+                    編集
+                  </Button>
+                </Td>
+                <Td>
+                  <Button colorScheme="green" variant="outline" size="sm">
+                    削除
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    );
+  }
+});
