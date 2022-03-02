103326e75d08b2416624f14fec88ea64482dc6bb
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-02 15:37:51 +0900

    カレンダーオプション追加

--- client/src/components/molucules/calendar.tsx
+++ client/src/components/molucules/calendar.tsx
@@ -17,8 +17,15 @@ export const Calendar: FC = memo(() => {
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         // locale='ja' // 日本語化
-        eventColor="#48BB78"
+        eventColor="#48BB78" 
+        businessHours={true} // 休日に色をつけるかどうか
+        editable={true} // イベントを編集できるかどうか(移動可能に)
+        contentHeight="auto" // カレンダーの高さ
+        dayMaxEvents={3} // 1日に表示できるイベント数
         events={[
+          { title: "GraphQL学習する", start: "2022-03-01" },
+          { title: "GraphQL学習する", start: "2022-03-01" },
+          { title: "GraphQL学習する", start: "2022-03-01" },
           { title: "GraphQL学習する", start: "2022-03-01" },
           // endに指定した日付は含まないので注意
           { title: "Node.js", start: "2022-03-10", end: "2022-03-15" },
