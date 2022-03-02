2bf12d05618f198d000efe723197d894ed642a57
kosuke-17 <tamutamu17@icloud.com>
2022-03-02 17:05:36 +0900

    処理の命名変更

--- server/src/graphql/resolvers/userLeafs/mutations.ts
+++ server/src/graphql/resolvers/userLeafs/mutations.ts
@@ -1,10 +1,9 @@
-import { UserLeafs, Users } from "../../../models/User.model";
+import { UserLeafs } from "../../../models/User.model";
 import { success } from "../responseStatus";
-import { UserTechLeafsType } from "../types";
 
 const userLeafsMutation = {
   /**
-   * 習得技術追加.
+   * 習得技術更新.
    *
    * @remarks upsertがあることで指定したフィールドが配列でなかったり、指定したフィールドが存在しなかった場合にエラーが発生する
    *
@@ -12,7 +11,7 @@ const userLeafsMutation = {
    * @returns success : successステータス,技術を習得したユーザー
    * @returns error : errorステータス
    */
-  addUserTechLeafs: async (_: any, { techLeaf }: { techLeaf: any }) => {
+  updateUserTechLeafs: async (_: any, { techLeaf }: { techLeaf: any }) => {
     const { techTreeId, achievementRate, techLeafIds, userId } = techLeaf;
     try {
       const result = await UserLeafs.findOneAndUpdate(
@@ -27,31 +26,6 @@ const userLeafsMutation = {
           arrayFilters: [{ "techLeafInfo.techTreeId": { $eq: techTreeId } }],
           upsert: true,
           new: true,
-        }
-      );
-      return success(result);
-    } catch (e) {
-      // 必須のデータがnullだとエラーを返す
-      return { status: "error" };
-    }
-  },
-  /**
-   * 習得技術削除.
-   *
-   * @param user - ユーザー情報
-   * @returns success : successステータス,習得技術を削除したユーザー
-   * @returns error : errorステータス
-   */
-  removeUserTechLeafs: async (
-    _: any,
-    { user }: { user: UserTechLeafsType }
-  ) => {
-    const { _id, techLeafId } = user;
-    try {
-      const result = await Users.findByIdAndUpdate(
-        { _id: _id },
-        {
-          $pull: { have_techLeafs: techLeafId },
         }
       );
       return success(result);
