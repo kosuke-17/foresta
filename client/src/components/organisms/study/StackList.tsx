580a81e4b33acb63e4a606cd90b20f7e83ff000e
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-03-03 18:21:15 +0900

    同じ技術内容の学習時間を追加することができた

--- client/src/components/organisms/study/StackList.tsx
+++ client/src/components/organisms/study/StackList.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Center,
   Tab,
   Table,
@@ -16,7 +15,10 @@ import {
 } from "@chakra-ui/react";
 
 import { memo } from "react";
-import { useGetAllStudyStackByUserIdQuery } from "../../../types/generated/graphql";
+import {
+  useGetAllStudyStackByUserIdQuery,
+  // useGetSkillTagIdQuery,
+} from "../../../types/generated/graphql";
 import { StudyModal } from "../../molucules/StudyModal";
 
 /**
@@ -29,7 +31,27 @@ export const StackList = memo(() => {
     //仮のユーザーID
     variables: { userId: "621c795fea18ffdb80e66462" },
   });
-  console.log(data);
+
+  const timeStackDatas = new Array<number>();
+  const skillTagIdtackDatas = new Array<string>();
+
+  if (data) {
+    const stackDatas = data.getAllStudyStackByUserId;
+    //カテゴリ名が被っていたら学習時間を合わせて排除
+    for (let i = 0; i < stackDatas.length; i++) {
+      let answer = stackDatas[i].timeStack;
+      for (let j = i + 1; j < stackDatas.length; j++) {
+        if (stackDatas[i].skillTagId === stackDatas[j].skillTagId) {
+          answer = answer + stackDatas[j].timeStack;
+          i = i + 1;
+        }
+      }
+      timeStackDatas.push(answer);
+      skillTagIdtackDatas.push(stackDatas[i].skillTagId);
+      console.log(timeStackDatas);
+      console.log(skillTagIdtackDatas);
+    }
+  }
 
   if (loading) {
     return <p>Loading...</p>;
