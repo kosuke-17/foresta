bf3cc669bbd4e1d0982659e0af0bba07e69a6681
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-03 18:35:11 +0900

    カレンダーに実データを渡して表示させた。

--- client/src/components/molucules/calendar.tsx
+++ client/src/components/molucules/calendar.tsx
@@ -1,13 +1,36 @@
 import { FC, memo } from "react";
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, { addDays, EventInput } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Box } from "@chakra-ui/react";
 
-export const Calendar: FC = memo(() => {
+import type { Todo } from "../../types/generated/graphql";
+
+type Props = {
+  todos: Pick<Todo, "id" | "title" | "startedAt" | "finishedAt">[] | undefined;
+};
+
+/**
+ * カレンダーを表示するコンポーネント.
+ *
+ * @param todos Todoの配列
+ */
+export const Calendar: FC<Props> = memo((props) => {
+  const { todos } = props;
+
+  // propsで渡ってきたtodoデータからeventデータを作成
+  const events = todos?.map((todo) => {
+    return {
+      title: todo.title,
+      start: todo.startedAt,
+      end: todo.finishedAt ? addDays(new Date(todo.finishedAt), 1) : null,
+      allDay: true,
+    } as EventInput; // fullcalendarのEvent用の型に変換
+  });
+
   return (
     <Box
-      padding="1rem 10rem"
+      padding="1rem 3rem"
       margin="1rem"
       bg="#f0fff4"
       border="1px solid #6BE4A3"
@@ -17,20 +40,12 @@ export const Calendar: FC = memo(() => {
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         // locale='ja' // 日本語化
-        eventColor="#48BB78" 
+        eventColor="#48BB78"
         businessHours={true} // 休日に色をつけるかどうか
         editable={true} // イベントを編集できるかどうか(移動可能に)
         contentHeight="auto" // カレンダーの高さ
         dayMaxEvents={3} // 1日に表示できるイベント数
-        events={[
-          { title: "GraphQL学習する", start: "2022-03-01" },
-          { title: "GraphQL学習する", start: "2022-03-01" },
-          { title: "GraphQL学習する", start: "2022-03-01" },
-          { title: "GraphQL学習する", start: "2022-03-01" },
-          // endに指定した日付は含まないので注意
-          { title: "Node.js", start: "2022-03-10", end: "2022-03-15" },
-        ]}
-        // selectable="true"
+        events={events || []} // イベントを設定
       />
     </Box>
   );
