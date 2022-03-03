5ad1ac1ba40372ce4111cb8c84149e14e09aec2a
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-03 20:02:55 +0900

    githubアイコン追加

--- client/src/components/organisms/aboutMe/Public.tsx
+++ client/src/components/organisms/aboutMe/Public.tsx
@@ -1,6 +1,9 @@
-import { memo, FC, useState } from "react";
-import { Button, Box } from "@chakra-ui/react";
+import { memo, FC } from "react";
+import { Button, Box, Flex } from "@chakra-ui/react";
 import { useGetUserByIdQuery } from "../../../types/generated/graphql";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { MarkGithubIcon } from "@primer/octicons-react";
 
 export const Public: FC = memo(() => {
   /**
@@ -8,10 +11,11 @@ export const Public: FC = memo(() => {
    * @remarks 取得情報:名前、職種、GitHub
    */
   const { loading, error, data } = useGetUserByIdQuery({
+    //idは実際cookieから取得
     variables: { id: "621b15dd3200d51bb64b2d42" },
   });
 
-  //useState付けるとデータ入る前にレンダリングされて終わる
+  //useState付けるとデータ入る前にレンダリングされて終わるみたい
   const user = data?.getUserById;
 
   //読み込み中時の表示
@@ -27,20 +31,40 @@ export const Public: FC = memo(() => {
    */
   return (
     <>
-      <Box background={"green.100"} m={10} p={20} rounded={20} boxShadow="md">
+      <Box
+        background={"green.100"}
+        m={10}
+        p={20}
+        rounded={20}
+        boxShadow="md"
+        textAlign="center"
+      >
         {user && (
           <>
-            <div>氏名:{user.name}</div>
+            <Flex justifyContent="center">
+              <_Name>氏名:{user.name}</_Name>
+              {user.githubURL && (
+                <_Icon>
+                  <a
+                    href={`https://github.com/${user.githubURL}`}
+                    target="blank"
+                  >
+                    <MarkGithubIcon size={16} />
+                  </a>
+                </_Icon>
+              )}
+            </Flex>
             <div>{user.jobType}</div>
             <div>
-              <Button colorScheme="teal" size="sm">
-                このユーザの学習記録
+              <Button backgroundColor="green.400" size="md" textColor="white">
+                {/* 仮のリンク */}
+                <Link to={"/study"}>このユーザの学習記録</Link>
               </Button>
             </div>
             <div>
-              <Button colorScheme="teal" size="sm">
-                GitHubアカウント
-              </Button>
+              {/* <Button backgroundColor="green.400" size="md" textColor="white"> */}
+
+              {/* </Button> */}
             </div>
             <div>技術進捗ツリーチェック項目</div>
             <div>制作物一覧</div>
@@ -58,3 +82,16 @@ export const Public: FC = memo(() => {
     </>
   );
 });
+
+const _Name = styled.div`
+  font-weight: bold;
+  font-size: 25px;
+`;
+
+const _Icon = styled.div`
+  margin-top: 5px;
+  margin-left: 5px;
+  :hover {
+    color: gray;
+  }
+`;
