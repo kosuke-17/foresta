1e9e8309161f620b85a94b92223593a1b605a256
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-03 10:25:11 +0900

    テストデータを入れてみた

--- client/src/container/templates/AboutMe.tsx
+++ client/src/container/templates/AboutMe.tsx
@@ -1,10 +1,27 @@
 import { FC } from "react";
 import { Private } from "../../components/organisms/aboutMe/Private";
 import { Public } from "../../components/organisms/aboutMe/Public";
+import { ModalSet } from "../../components/molucules/ModalSet";
 
 export const AboutMe: FC = () => {
+  const testAction1 = () => {
+    alert("ボタン1が押された");
+  };
+  const testAction2 = () => {
+    alert("ボタン2が押された");
+  };
+  const testArray = [
+    { name: "test1", action: testAction1 },
+    { name: "test2", action: testAction2 },
+  ];
+
   return (
     <>
+      <ModalSet
+        openBtnName="テストボタン"
+        contents="あああ"
+        actionBtnArray={testArray}
+      />
       <Public />
       <Private />
     </>
