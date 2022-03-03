1b1c6962afef6d36b1b7e3b5726e7ea978dc05ea
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-03 10:10:57 +0900

    全てのTodoを取得するクエリを作成し、codegenした

--- client/src/queries/query.ts
+++ client/src/queries/query.ts
@@ -12,3 +12,15 @@ import { gql } from "@apollo/client";
 //     }
 //   }
 // `;
+
+gql`
+  query GetAllTodoByUser($userId: String) {
+     todos: getAllTodoByUser(userId: $userId) {
+      id
+      title
+      startedAt
+      finishedAt
+      isStatus
+    }
+  }
+`;
