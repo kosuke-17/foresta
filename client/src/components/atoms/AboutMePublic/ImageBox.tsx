0a616e6751b46d80bbcd67f722affe2f749d586a
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-04 11:26:48 +0900

    画像用コンポーネント作成

---
+++ client/src/components/atoms/AboutMePublic/ImageBox.tsx
@@ -0,0 +1,13 @@
+import { memo, FC } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+
+type Props = {
+  imageUrl: string;
+};
+
+/**
+ * サイト画面表示用.
+ */
+export const ImageBox: FC<Props> = memo(({ imageUrl }) => {
+  return <></>;
+});
