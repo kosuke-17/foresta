1e35876df036c3b8c6072d308fe499cc97a77fb0
yukimaru5447 <w140113@gmail.com>
2022-03-05 13:29:01 +0900

    atomsにログインボタンを追加

---
+++ client/src/components/atoms/auth/LoginButton.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { Center, SimpleGrid } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+
+type Login = {
+  doLogin: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export const LoginButton: FC<Login> = ({ doLogin }) => {
+  return (
+    <Center>
+      <SimpleGrid columns={1}>
+        <Button w={72} colorScheme="green" variant="solid" onClick={doLogin}>
+          ログイン
+        </Button>
+      </SimpleGrid>
+    </Center>
+  );
+};
