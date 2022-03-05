52ccdb4d6d1ffaa2301f72b1323570c31b34d90a
yukimaru5447 <w140113@gmail.com>
2022-03-05 13:23:10 +0900

    atomsにメールアドレスの入力フォームを追加

---
+++ client/src/components/atoms/aboutMePrivate/EmailInput.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ChangeEvent } from "react";
+import { Center, Text, Input, SimpleGrid } from "@chakra-ui/react";
+
+type InputForm = {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const EmailInput: FC<InputForm> = ({ value, onChange }) => {
+  return (
+    <Center mt={8}>
+      <SimpleGrid columns={1}>
+        <Text mb={2} fontSize="sm">
+          メールアドレス
+        </Text>
+        <Input w={72} bg="white" value={value} onChange={(e) => onChange(e)} />
+      </SimpleGrid>
+    </Center>
+  );
+};
