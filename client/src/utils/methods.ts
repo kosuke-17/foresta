2b0b55684617f72261aba6dc4dd6f2d1ac6cb3e6
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-07 16:19:30 +0900

    Todo用にフォーマットされた日付を返すメソッドをutilsに移動

--- client/src/utils/methods.ts
+++ client/src/utils/methods.ts
@@ -20,3 +20,24 @@ export const getFormattedDate = (date: Date) => {
 export const getFormattedDateWithoutYear = (date: Date) => {
   return format(date, "MM/dd");
 };
+
+/**
+  * Todo用にフォーマットされた日付を返すメソッド.
+  *
+  * @remarks
+  * 終了日が設定されている場合とされてない場合で場合分け
+  * @param startedA - 対象のTodoの開始日
+  * @param finishedAt - 対象のTodoの終了日
+  * @returns フォーマットされた日付
+  */
+export const getformattedTodoDate = (startedAt: string, finishedAt: string): string => {
+  if (startedAt && finishedAt) {
+    return `${getFormattedDate(
+      new Date(startedAt),
+    )}-${getFormattedDateWithoutYear(new Date(finishedAt))}`;
+  } else if (startedAt) {
+    return `${getFormattedDate(new Date(startedAt))}`;
+  } else {
+    return "未定";
+  }
+};
