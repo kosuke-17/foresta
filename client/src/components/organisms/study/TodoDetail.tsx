b62cd81ac4b83265f23110cd3d8fb3f170bbc138
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-08 18:18:19 +0900

    編集モードとの切り替え単位を変更

--- client/src/components/organisms/study/TodoDetail.tsx
+++ client/src/components/organisms/study/TodoDetail.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useCallback, useState } from "react";
+import { FC, memo, useState } from "react";
 import {
   Button,
   Modal,
@@ -10,32 +10,36 @@ import {
   CloseButton,
   Flex,
   Box,
-  Input,
 } from "@chakra-ui/react";
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
 import styled from "styled-components";
 import { useGetTodoByIdQuery } from "../../../types/generated/graphql";
 import { getformattedTodoDate } from "../../../utils/methods";
+import { TodoDetailEdit } from "./TodoDetailEdit";
 
 type Props = {
-  todoId: string;
+  todoId: string; 
   isOpen: boolean;
   onClose: () => void;
 };
 
+/**
+ * Todoの詳細を表示するコンポーネント.
+ */
 export const TodoDetail: FC<Props> = memo((props) => {
   const { todoId, isOpen, onClose } = props;
 
+  // 編集モードかどうか
   const [isEditing, setIsEditing] = useState(false);
 
   const { data, error, loading } = useGetTodoByIdQuery({
     variables: {
       todoId,
     },
-    fetchPolicy: "cache-and-network"
+    fetchPolicy: "cache-and-network",
   });
 
-  const [title, setTitle] = useState(data?.todo.title);
+  const todo = data?.todo;
 
   if (error) {
     return <div>{error.message}</div>;
@@ -45,89 +49,69 @@ export const TodoDetail: FC<Props> = memo((props) => {
     return <div>loading...</div>;
   }
 
-  console.log(data);
-  const todo = data?.todo;
-
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose} isCentered={true} size="xl">
         <ModalOverlay />
         <ModalContent bg="green.50">
-          <ModalHeader>
-            <Flex justify="space-between">
-              <CloseButton />
-              <Flex gap={1}>
-                {isEditing ? (
+          {isEditing ? (
+            // 編集モード
+            <TodoDetailEdit
+              todo={todo}
+              isEditing={isEditing}
+              setIsEditing={setIsEditing}
+            />
+          ) : (
+            <>
+              <ModalHeader>
+                <Flex justify="space-between">
+                  <CloseButton onClick={onClose} />
+                  <Flex gap={1}>
+                    <>
+                      <Button
+                        colorScheme="green"
+                        size="sm"
+                        onClick={() => setIsEditing(true)}
+                      >
+                        <EditIcon />
+                        編集
+                      </Button>
+                      <Button colorScheme="green" size="sm">
+                        <DeleteIcon />
+                        削除
+                      </Button>
+                    </>
+                  </Flex>
+                </Flex>
+              </ModalHeader>
+
+              <ModalBody padding="0 4rem">
+                {todo && (
                   <>
-                    <Button
-                      colorScheme="green"
-                      size="sm"
-                      onClick={() => alert("編集完了")}
-                    >
-                      保存
-                    </Button>
-                    <Button
-                      colorScheme="green"
-                      size="sm"
-                      onClick={() => setIsEditing(false)}
-                    >
-                      キャンセル
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      colorScheme="green"
-                      size="sm"
-                      onClick={() => setIsEditing(true)}
-                    >
-                      <EditIcon />
-                      編集
-                    </Button>
-                    <Button colorScheme="green" size="sm">
-                      <DeleteIcon />
-                      削除
-                    </Button>
+                    <Heading as="h2" size="lg" mb={5}>
+                      {todo.title}
+                    </Heading>
+                    <div>
+                      <_Label>日付: </_Label>
+
+                      {getformattedTodoDate(todo.startedAt, todo.finishedAt)}
+                    </div>
+                    <div>
+                      <_Label>ステータス: </_Label>
+                      {todo.isStatus ? "完了" : "未完了"}
+                    </div>
+
+                    <Box mt={5}>
+                      <_Label>メモ</_Label>
+                      <_MemoContent>
+                        {todo.description ? todo.description : " "}
+                      </_MemoContent>
+                    </Box>
                   </>
                 )}
-              </Flex>
-            </Flex>
-          </ModalHeader>
-
-          <ModalBody padding="0 4rem">
-            {todo && (
-              <>
-                <Heading as="h2" size="lg" mb={5}>
-                  {isEditing ? (
-                    <>
-                      <Input
-                        focusBorderColor="green.200"
-                        value={todo.title}
-                      />
-                    </>
-                  ) : (
-                    <>{todo.title}</>
-                  )}
-                </Heading>
-                <div>
-                  <_Label>日付: </_Label>
-
-                  {getformattedTodoDate(todo?.startedAt, todo?.finishedAt)}
-                </div>
-                <div>
-                  <_Label>ステータス: </_Label>
-                  {todo?.isStatus ? "完了" : "未完了"}
-                </div>
-
-                <Box mt={5}>
-                  <_Label>メモ</_Label>
-                  <_MemoContent>
-                    {todo?.description ? todo?.description : " "}
-                  </_MemoContent>
-                </Box>
-              </>
-            )}
-          </ModalBody>
+              </ModalBody>
+            </>
+          )}
         </ModalContent>
       </Modal>
     </>
