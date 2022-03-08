537eda9dc8d737c361e56581cf8f53b31fc61339
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-08 14:44:02 +0900

    EditMe→UserInfoコンポーネントにお引越し

--- client/src/components/molucules/editMe/UserInfo.tsx
+++ client/src/components/molucules/editMe/UserInfo.tsx
@@ -1,9 +1,24 @@
 import { memo, FC } from "react";
+import { TextInput } from "../../atoms/editMe/TextInput";
+import { useUserInfo } from "../../../hooks/editMe/useUserInfo";
+import { Button } from "@chakra-ui/react";
 
 /**
  * public部分基本情報編集画面.
  * @remarks 名前, 職種, github, (mail,pw,specsheetId)
  */
 export const UserInfo: FC = memo(() => {
-  return <></>;
+  const { handleSubmit, cancel, register, errors, onSubmit } =
+    useUserInfo("初期表示用データ");
+
+  return (
+    <>
+      <TextInput
+        registers={register("name")}
+        errorMessage={errors.name?.message}
+      />
+      <Button onClick={handleSubmit(onSubmit)}>送信</Button>
+      <Button onClick={cancel}>キャンセル</Button>
+    </>
+  );
 });
--- client/src/components/organisms/aboutMe/EditMe.tsx
+++ client/src/components/organisms/aboutMe/EditMe.tsx
@@ -1,26 +1,8 @@
 import { memo, FC } from "react";
-import { TextInput } from "../../atoms/editMe/TextInput";
-import { useUserInfo } from "../../../hooks/editMe/useUserInfo";
 
 /**
  * EditMe.
  */
 export const EditMe: FC = memo(() => {
-  const { handleSubmit, cancel, register, errors, onSubmit } =
-    useUserInfo("初期表示用データ");
-
-  return (
-    <>
-      <TextInput
-        registers={register("name")}
-        errorMessage={errors.name?.message}
-      />
-      <button type="button" onClick={handleSubmit(onSubmit)}>
-        送信
-      </button>
-      <button type="button" onClick={cancel}>
-        キャンセル
-      </button>
-    </>
-  );
+  return <></>;
 });
