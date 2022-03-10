e417528fb1993dc0c9bb16cf1fb5a0f666fd5e0e
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-10 13:27:42 +0900

    モーダル表示のテスト追加

--- client/__tests__/StackList.test.tsx
+++ client/__tests__/StackList.test.tsx
@@ -4,7 +4,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { GetAllStudyStackDocument } from "../src/types/generated/graphql";
-// import { LogListTable } from "../src/components/molucules/stackList/LogListTable";
 import { StackList } from "../src/components/organisms/study/StackList";
 import { MockedProvider } from "@apollo/react-testing";
 import userEvent from "@testing-library/user-event";
@@ -40,42 +39,21 @@ const mocks = [
 ];
 
 describe("学習リスト画面表示", () => {
-  // type NewType = {
-  //   data: GetAllStudyStackQuery; //更新情報データ
-  //   // data: StudyStack; //更新情報データ
-  //   success: () => void; //データの更新
-  // };
-
-  // type Props = NewType;
-  // let dummyProps: Props;
-
-  // const logListData = {
-  //   getAllStudyStack: Array<{
-  //     id: "1";
-  //     createdAt: "2022-02-02";
-  //     skillTagId: "React";
-  //     timeStack: 180;
-  //     content: "理解した";
-  //     userId: "2";
-  //   }>(),
-  // };
-
-  // const successData = () => {
-  //   alert("成功");
-  // };
   it("学習リスト画面が表示されるか", () => {
-    // dummyProps = {
-    //   data: logListData,
-    //   success: successData,
-    // };
-
     render(
       <MockedProvider mocks={mocks} addTypename={false}>
-        {/* <LogListTable {...dummyProps} /> */}
         <StackList />
       </MockedProvider>,
     );
-    expect(screen.getByText("学習リスト")).toBeInTheDocument();
-    userEvent.click(screen.getByText("更新情報"));
+
+    expect(screen.getByText("学習リスト")).toBeInTheDocument(); //学習リスト画面表示
+    userEvent.click(screen.getByText("更新情報")); //更新情報クリックで表示変更
+    userEvent.click(screen.getByText("記録")); //記録ボタンクリック
+    expect(screen.getByText("記録追加")).toBeInTheDocument(); //モーダル表示
+    userEvent.click(screen.getByText("記録する")); //記録するボタンクリック
+    expect(screen.getByText("学習リスト")).toBeInTheDocument(); //学習リスト画面に戻る
+    expect(screen.getByText("記録追加")).toBeInTheDocument(); //モーダル表示
+    userEvent.click(screen.getByText("キャンセル")); //キャンセルボタンクリック
+    expect(screen.getByText("学習リスト")).toBeInTheDocument(); //学習リスト画面に戻る
   });
 });
