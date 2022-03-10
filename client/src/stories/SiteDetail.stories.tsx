41fea18d210ed08b7a510ac327e10a64ea7048fa
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-10 16:52:12 +0900

    制作物詳細コンポーネントストーリーブック追加

---
+++ client/src/stories/SiteDetail.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { SiteDetail } from "../components/molucules/aboutMePublic/SiteDetail";
+
+export default {
+  component: SiteDetail,
+} as ComponentMeta<typeof SiteDetail>;
+
+export const Default: ComponentStoryObj<typeof SiteDetail> = {
+  args: {
+    siteItem: {
+      title: "Default",
+      description: "Default",
+      img: "Default.jpg",
+      portfolioURL: "Default",
+    },
+  },
+  storyName: "デフォルト",
+};
