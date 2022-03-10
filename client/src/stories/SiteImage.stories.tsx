d158f4d43d3d179d089795a776d9d379da24635d
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-10 16:52:21 +0900

    制作物画像コンポーネントストーリーブック追加

---
+++ client/src/stories/SiteImage.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { SiteImage } from "../components/atoms/aboutMePublic/SiteImage";
+
+export default {
+  component: SiteImage,
+} as ComponentMeta<typeof SiteImage>;
+
+export const Default: ComponentStoryObj<typeof SiteImage> = {
+  args: {
+    imageUrl: "Default",
+    siteName: "Default",
+  },
+  storyName: "デフォルト",
+};
