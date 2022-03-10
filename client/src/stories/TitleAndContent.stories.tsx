491c332164358e4c23e081a4dc1eb73a8e4dd4b7
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-10 16:52:44 +0900

    タイトルとコンテンツコンポーネントストーリーブック追加

---
+++ client/src/stories/TitleAndContent.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { TitleAndContent } from "../components/atoms/aboutMePrivate/TitleAndContent";
+
+export default {
+  component: TitleAndContent,
+} as ComponentMeta<typeof TitleAndContent>;
+
+export const Default: ComponentStoryObj<typeof TitleAndContent> = {
+  args: {
+    title: "Default",
+    content: "Default",
+  },
+  storyName: "デフォルト",
+};
