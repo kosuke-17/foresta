f945c5ce0ccec70f248630f89f143aa52252b832
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-10 16:52:02 +0900

    アコーディオンコンポーネントストーリーブック追加

---
+++ client/src/stories/AccordionContent.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { AccordionContent } from "../components/molucules/AccordionContent";
+
+export default {
+  component: AccordionContent,
+} as ComponentMeta<typeof AccordionContent>;
+
+export const large: ComponentStoryObj<typeof AccordionContent> = {
+  args: {
+    title: "large",
+    content: <p>large</p>,
+    size: "lg",
+  },
+  storyName: "大きい文字",
+};
+
+export const small: ComponentStoryObj<typeof AccordionContent> = {
+  args: {
+    title: "small",
+    content: <p>small</p>,
+    size: "sm",
+  },
+  storyName: "小さい文字",
+};
