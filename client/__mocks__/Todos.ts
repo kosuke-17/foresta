bb894a18ad4c0d6278a5bd1b0b35ef2aaebe0d3f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-11 15:14:37 +0900

    Todo取得のクエリのモック修正

--- client/__mocks__/Todos.ts
+++ client/__mocks__/Todos.ts
@@ -6,32 +6,34 @@ export const todoMocks = [
     request: {
       query: GetAllTodoByUserDocument, // Codegenで生成されたクエリ Documentで終わるもの。
       variables: {
-        userId: "test"
+        userId: "621f1cba386085f036353ecd"
       }
     },
-
     result: {
       // データをモック
       data: {
         __typename: "Query",
-        todos: [
-          {
-            __typename: "Todo",
-            id: "aaa",
-            title: "テスト1",
-            startedAt: "2022-03-10",
-            finishedAt: "2022-03-12",
-            isStatus: false,
-          },
-          {
-            __typename: "Todo",
-            id: "bbb",
-            title: "テスト",
-            startedAt: "2022-03-15",
-            finishedAt: "2022-03-16",
-            isStatus: true,
-          },
-        ],
+        todos: {
+          __typename: "ResponseTodoArr",
+          node: [
+            {
+              __typename: "Todo",
+              id: "aaa",
+              title: "テスト1",
+              startedAt: "2022-03-10",
+              finishedAt: "2022-03-12",
+              isStatus: false,
+            },
+            {
+              __typename: "Todo",
+              id: "bbb",
+              title: "テスト2",
+              startedAt: "2022-03-15",
+              finishedAt: "2022-03-16",
+              isStatus: true,
+            },
+          ],
+        }
       },
     },
   },
@@ -46,17 +48,19 @@ export const todoMocks = [
       // データをモック
       data: {
         __typename: "Query",
-        todo: [
+        todo: {
+          __typename: "ResponseTodo",
+          node:
           {
             __typename: "Todo",
             id: "test",
-            title: "テスト1",
+            title: "テスト",
             description: "メモ",
             startedAt: "2022-03-10",
             finishedAt: "2022-03-12",
             isStatus: false,
           },
-        ],
+        },
       },
     },
   },
