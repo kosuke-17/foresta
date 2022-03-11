8070102830ed15bc18e6234fca4142ff52f77eb6
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-11 15:16:20 +0900

    余分なコード削除

--- client/src/stories/Todos/TodosArea.stories.tsx
+++ client/src/stories/Todos/TodosArea.stories.tsx
@@ -1,12 +1,9 @@
 import type { ComponentStoryObj, ComponentMeta } from "@storybook/react";
 import { todoMocks } from "../../../__mocks__/Todos";
-import { TodoList } from '../../components/organisms/study/TodoList';
 import { TodosArea } from "../../components/organisms/study/TodosArea";
-import { GetAllTodoByUserDocument } from "../../types/generated/graphql";
 
 export default {
   component: TodosArea,
-
 } as ComponentMeta<typeof TodosArea>;
 
 export const Default: ComponentStoryObj<typeof TodosArea> = {
