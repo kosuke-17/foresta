2bf8d1450dca4cc2650e4000610099b5290fd169
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-12 16:54:02 +0900

    学習リスト用テキストボックス作成

---
+++ client/src/components/atoms/study/StackInput.tsx
@@ -0,0 +1,28 @@
+import { FC, memo } from "react";
+import { Box, FormLabel, Input } from "@chakra-ui/react";
+
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  registers: any; //入力値登録
+  errorMessage?: string | undefined; //エラーメッセージ
+  placeholder: string; //プレイスホルダー名
+  type: string; //テキストタイプ
+  label: string; //ラベル名
+};
+
+/**
+ * テキストボックスコンポ―ネント.
+ */
+export const StackInput: FC<Props> = memo((props) => {
+  const { registers, errorMessage, placeholder, label, type } = props;
+
+  return (
+    <>
+      <FormLabel>{label}</FormLabel>
+      <Input type={type} placeholder={placeholder} {...registers} />
+      <Box textColor="red" fontSize="xs">
+        {errorMessage}
+      </Box>
+    </>
+  );
+});
