bc2fb847e8cadea1a2f35f94432dc095b2b9dc09
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-12 17:47:30 +0900

    useFormに変更したため、それぞれのカスタムフックも修正

--- client/src/hooks/study/useAddStack.tsx
+++ client/src/hooks/study/useAddStack.tsx
@@ -1,57 +1,67 @@
 import { useToast } from "@chakra-ui/react";
 import { useCallback } from "react";
 import { useCookies } from "react-cookie";
+import { UseFormReset } from "react-hook-form";
 import {
   useAddStudyStackMutation,
   GetAllStudyStackDocument,
 } from "../../types/generated/graphql";
 
+type AddStack = {
+  createdAt: string;
+  skillTagId: string;
+  timeStack: number;
+  content: string;
+};
+
 /**
- * 学習記録を記録するフックス
- * @param createdAt - 記録日
- * @param skillTagId - 学習技術
- * @param timeStack - 学習時間
- * @param content - メモ
- * @param onClose - モーダルを閉じるメソッド
- * @returns
- * addStack : 学習を記録する
+ * 学習記録を追加するフックス.
+ * @param onClose
+ * @param reset
+ * @returns addStack - 記録するメソッド
  */
 export const useAddStack = (
-  createdAt: string,
-  skillTagId: string,
-  timeStack: number,
-  content: string,
   onClose: () => void,
+  reset: UseFormReset<AddStack>,
 ) => {
   //クッキー情報取得
   const [cookies] = useCookies();
   //トーストアラート
   const toast = useToast();
-  //記録追加メソッド（リフェッチ機能）
+  //記録追加ミューテーション（リフェッチ機能）
   const [addStudyStack] = useAddStudyStackMutation({
-    variables: {
-      stack: {
-        createdAt,
-        skillTagId,
-        timeStack,
-        content,
-        //一旦指定のユーザー
-        userId: cookies.ForestaID,
-      },
-    },
     refetchQueries: [GetAllStudyStackDocument], //データを表示するクエリーのDocument
   });
 
-  //記録追加
-  const addStack = useCallback(async () => {
-    const addStackData = await addStudyStack();
-    if (addStackData.data?.addStudyStack.status === "success") {
-      toast({ title: "学習を記録しました", status: "success" });
-      onClose();
-    } else if (addStackData.data?.addStudyStack.status === "error") {
-      toast({ title: "記録に失敗しました", status: "error" });
-      onClose();
-    }
-  }, [addStudyStack, onClose, toast]);
+  /**
+   * 学習を記録するメソッド
+   * @param data - 入力した内容
+   */
+  const addStack = useCallback(
+    async (data: AddStack) => {
+      const addStackData = await addStudyStack({
+        variables: {
+          stack: {
+            createdAt: data.createdAt,
+            skillTagId: data.skillTagId,
+            timeStack: data.timeStack,
+            content: data.content,
+            userId: cookies.ForestaID,
+          },
+        },
+      });
+      if (addStackData.data?.addStudyStack.status === "success") {
+        toast({ title: "学習を記録しました", status: "success" });
+        onClose();
+        reset();
+      } else if (addStackData.data?.addStudyStack.status === "error") {
+        toast({ title: "記録に失敗しました", status: "error" });
+        onClose();
+        reset();
+      }
+    },
+    [addStudyStack, cookies.ForestaID, onClose, reset, toast],
+  );
+
   return { addStack };
 };
