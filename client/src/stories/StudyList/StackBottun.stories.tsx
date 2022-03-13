27be6e4e8aaf0ed29daeba8c188a56eaeb2b2658
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-14 08:55:48 +0900

    StoryBookにコンポーネント登録

---
+++ client/src/stories/StudyList/StackBottun.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { StackButton } from "../../components/atoms/study/StackBotton";
+
+export default {
+  component: StackButton,
+} as ComponentMeta<typeof StackButton>;
+
+export const AddStack: ComponentStoryObj<typeof StackButton> = {
+  args: {
+    title: "記録する",
+  },
+  storyName: "学習リストを記録するボタン",
+};
+export const UpdateStack: ComponentStoryObj<typeof StackButton> = {
+  args: {
+    title: "編集する",
+  },
+  storyName: "学習リストを編集するボタン",
+};
+export const DeleteStack: ComponentStoryObj<typeof StackButton> = {
+  args: {
+    title: "削除する",
+  },
+  storyName: "学習リストを削除するボタン",
+};
