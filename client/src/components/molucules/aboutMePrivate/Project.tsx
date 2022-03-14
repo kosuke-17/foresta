5a0201aa0fcb460b01727d96c1fc15169a7e7671
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-14 15:46:23 +0900

    開発経験もDBから取得に変更

--- client/src/components/molucules/aboutMePrivate/Project.tsx
+++ client/src/components/molucules/aboutMePrivate/Project.tsx
@@ -1,76 +1,143 @@
-import { memo, FC, useState } from "react";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { memo, FC } from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Spinner,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
+import { useCookies } from "react-cookie";
+import styled from "styled-components";
 
 import { TableFlexItem } from "../../atoms/TableFlexItem";
+import { returnCodeToBr } from "../../../utils/methods";
+import {
+  SpecProjectSheet,
+  useGetSheetProjectByUserIdQuery,
+} from "../../../types/generated/graphql";
+import { AccordionContent } from "../AccordionContent";
 
 /**
  * 開発経験.
  */
 export const Project: FC = memo(() => {
-  //テストデータ
-  const [boxItems] = useState(["Windows", "Mac", "Linux"]);
+  //cookieからID取得
+  const [cookies] = useCookies();
 
+  /**
+   * スペックシート開発経験取得.
+   */
+  const { data, loading, error } = useGetSheetProjectByUserIdQuery({
+    variables: {
+      userId: cookies.ForestaID,
+    },
+  });
+  const projectData = data?.projects.node.project as Array<SpecProjectSheet>;
+
+  //読み込み中時の表示
+  if (loading) {
+    return (
+      <Flex justifyContent="center">
+        <Spinner color="green.400" />
+      </Flex>
+    );
+  }
+  //エラー時の表示
+  if (error) {
+    return <Flex justifyContent="center">Error</Flex>;
+  }
   return (
     <>
-      <Table variant="striped" colorScheme="gray">
-        <Thead>
-          <Tr>
-            <Th>項目名</Th>
-            <Th>内容</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>プロジェクト名</Td>
-            <Td>ほげほげサイト</Td>
-          </Tr>
-          <Tr>
-            <Td>期間</Td>
-            <Td>0000ヵ月</Td>
-          </Tr>
-          <Tr>
-            <Td>担当役割</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>チーム人数</Td>
-            <Td>00</Td>
-          </Tr>
-          <Tr>
-            <Td>動作環境(OS)</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>言語</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>フレームワーク</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>ライブラリ</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>ツール・その他</Td>
-            <Td>
-              <TableFlexItem itemArray={boxItems} />
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
-      <div>プロジェクト詳細</div>
+      {projectData &&
+        projectData.map((project) => (
+          <div key={project?.id}>
+            <AccordionContent
+              title={project?.name}
+              content={
+                <>
+                  <Table variant="striped" colorScheme="gray">
+                    <Thead>
+                      <Tr>
+                        <Th>項目名</Th>
+                        <Th>内容</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      <Tr>
+                        <Td>プロジェクト名</Td>
+                        <Td>{project?.name}</Td>
+                      </Tr>
+                      <Tr>
+                        <Td>期間</Td>
+                        <Td>
+                          {project?.startedAt}
+                          {project?.finishedAt}ヵ月
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>担当役割</Td>
+                        <Td>{project?.roleSharing}</Td>
+                      </Tr>
+                      <Tr>
+                        <Td>担当工程</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.devRoles} />
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>チーム人数</Td>
+                        <Td>{project?.memberCount}</Td>
+                      </Tr>
+                      <Tr>
+                        <Td>動作環境(OS)</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.operationEnvs} />
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>言語</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.languages} />
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>フレームワーク</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.frameworks} />
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>ライブラリ</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.libraries} />
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>ツール・その他</Td>
+                        <Td>
+                          <TableFlexItem itemArray={project?.otherTools} />
+                        </Td>
+                      </Tr>
+                    </Tbody>
+                  </Table>
+                  <_Detail>プロジェクト詳細</_Detail>
+                  <Box mx={10}>{returnCodeToBr(project?.content)}</Box>
+                </>
+              }
+              size="sm"
+            />
+          </div>
+        ))}
     </>
   );
 });
+
+const _Detail = styled.div`
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
