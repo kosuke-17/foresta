0bc12cba4d6ce96d485931551b624afde22893f8
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-14 16:33:25 +0900

    編集のためにURLもDBから取得

--- client/src/components/molucules/editMe/UserUrls.tsx
+++ client/src/components/molucules/editMe/UserUrls.tsx
@@ -1,10 +1,11 @@
 import { memo, FC, Dispatch, SetStateAction } from "react";
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner, Flex } from "@chakra-ui/react";
 import { useCookies } from "react-cookie";
 
 import { SelectInput } from "../../atoms/editMe/SelectInput";
 import { TextInput } from "../../atoms/editMe/TextInput";
 import { useUserUrls } from "../../../hooks/editMe/useUserUrls";
+import { useGetUrlByIdQuery } from "../../../types/generated/graphql";
 
 type Props = {
   setMenuItem: Dispatch<SetStateAction<string>>; //menuItemセット用
@@ -18,6 +19,26 @@ type Props = {
 export const UserUrls: FC<Props> = memo(({ setMenuItem, onClose }) => {
   //cookieからID取得
   const [cookies] = useCookies();
+
+  const { data, loading, error } = useGetUrlByIdQuery({
+    variables: {
+      id: cookies.ForestaID,
+    },
+  });
+  console.dir(JSON.stringify(data?.urls.node.userUrls.user_urls));
+
+  //読み込み中時の表示
+  if (loading) {
+    return (
+      <Flex justifyContent="center">
+        <Spinner color="green.400" />
+      </Flex>
+    );
+  }
+  //エラー時の表示
+  if (error) {
+    return <Flex justifyContent="center">Error</Flex>;
+  }
   return (
     <>
       <Button type="button" onClick={onClose} _focus={{ boxShadow: "none" }}>
