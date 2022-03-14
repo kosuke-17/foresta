cfe676575e182b628c00de342af45c141614ba0d
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-14 12:24:17 +0900

    項目追加

--- client/src/components/atoms/editMe/SelectInput.tsx
+++ client/src/components/atoms/editMe/SelectInput.tsx
@@ -6,24 +6,27 @@ type Props = {
   registers: any; //value(テキストボックスの値と紐づけ用)
   errorMessage?: string | undefined; //エラーメッセージ(errors.registers?.messageの形で渡す)
   options: Array<string>; //セレクトボックスのoption
+  label?: string;
+  placeholder?: string;
 };
 
 /**
  * セレクトボックスコンポ―ネント.
  */
-export const SelectInput: FC<Props> = memo((props) => {
-  const { registers, errorMessage, options } = props;
-
-  return (
-    <>
-      {errorMessage}
-      <Select {...registers}>
-        {options.map((optionItem, index) => (
-          <option key={index} value={optionItem}>
-            {optionItem}
-          </option>
-        ))}
-      </Select>
-    </>
-  );
-});
+export const SelectInput: FC<Props> = memo(
+  ({ registers, errorMessage, options, label, placeholder }) => {
+    return (
+      <>
+        {errorMessage}
+        {label}
+        <Select {...registers} placeholder={placeholder}>
+          {options.map((optionItem, index) => (
+            <option key={index} value={optionItem}>
+              {optionItem}
+            </option>
+          ))}
+        </Select>
+      </>
+    );
+  },
+);
