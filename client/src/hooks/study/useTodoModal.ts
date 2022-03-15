69df563d5635c4c6357948c1ca7aadb14bf32c79
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-03-15 12:26:34 +0900

    Todoのモーダルを操作するためのhookを作成

---
+++ client/src/hooks/study/useTodoModal.ts
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { useDisclosure } from "@chakra-ui/react";
+import { TodoModalContext } from "../../Providers/TodoModalProvider";
+import { TodoData } from "../../types/types";
+
+/**
+ * Todoのモーダルを操作するためのhook.
+ */
+export const useTodoModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const {
+    setModalMode,
+    setTodo,
+  } = useContext(TodoModalContext);
+
+  /**
+   * Todo詳細表示のモーダルを開く.
+   * @param todo モーダルに表示するTodo
+   */
+  const openReadModal = (todo: TodoData) => {
+    setModalMode("read");
+    setTodo(todo);
+    onOpen();
+  };
+
+  /**
+   * Todo追加用のモーダルを開く.
+   */
+  const openAddModal = () => {
+    // モーダルのモードを追加モードに設定
+    setModalMode("add");
+    // Todoは空のオブジェクトを設定
+    setTodo({} as TodoData);
+    onOpen();
+  };
+
+  return { isOpen, onOpen, onClose, openReadModal, openAddModal };
+};
