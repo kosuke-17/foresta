72580102ccc993924d85e2097df768e5efc2665c
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-15 14:38:38 +0900

    グラフに学習リストのデータを反映できるようになった

--- client/src/components/organisms/study/StackTime.tsx
+++ client/src/components/organisms/study/StackTime.tsx
@@ -8,128 +8,85 @@ import "chartjs-plugin-datalabels";
 import { ChartOptions } from "chart.js";
 //日付を使うため
 import "chartjs-adapter-date-fns";
+import { useStackList } from "../../../hooks/study/useStackList";
 
 export const StackTime = memo(() => {
-  //今日のデータを取ってきて、今日を含んで過去6ヶ月にする
-  const labels = [
-    "1月",
-    "2月",
-    "3月",
-    "4月",
-    "5月",
-    "6月",
-    "7月",
-    "8月",
-    "9月",
-    "10月",
-    "11月",
-    "12月",
+  const { data } = useStackList();
+
+  //色サンプル（背景）
+  const backgroundColors = [
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(255, 206, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(153, 102, 255, 0.2)",
+    "rgba(255, 188, 188, 0.2)",
+    "rgba(255, 127, 127, 0.2)",
+    "rgba(127, 255, 255, 0.2)",
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(255, 206, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(153, 102, 255, 0.2)",
+    "rgba(255, 188, 188, 0.2)",
+    "rgba(255, 127, 127, 0.2)",
+    "rgba(127, 255, 255, 0.2)",
+  ];
+  //色サンプル（線）
+  const borderColors = [
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 188, 188, 1)",
+    "rgba(255, 127, 127, 1)",
+    "rgba(127, 255, 255, 1)",
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 188, 188, 1)",
+    "rgba(255, 127, 127, 1)",
+    "rgba(127, 255, 255, 1)",
   ];
 
-  const data = {
-    // labels: labels,
-    datasets: [
-      {
-        //techTreeのデータを取ってきて
-        label: "React",
-        data: [
-          {
-            x: "2021-05-06",
-            y: 50,
-          },
-          {
-            x: "2021-11-07",
-            y: 60,
-          },
-          {
-            x: "2021-12-07",
-            y: 20,
-          },
-          {
-            x: "2021-10-07",
-            y: 20,
-          },
-          {
-            x: "2021-9-07",
-            y: 20,
-          },
-          {
-            x: "2021-12-09 ",
-            y: 50,
-          },
-        ],
+  const firstDatasets = [];
+  const datasets = [];
 
-        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-        borderColor: ["rgba(255, 99, 132, 1)"],
-        borderWidth: 1,
-      },
-      {
-        label: "Vue",
-        data: [
-          {
-            x: "2021-05-06",
-            y: 50,
-          },
-          {
-            x: "2021-11-07",
-            y: 60,
-          },
-          {
-            x: "2021-12-07",
-            y: 20,
-          },
-          {
-            x: "2021-10-07",
-            y: 20,
-          },
-          {
-            x: "2021-9-07",
-            y: 20,
-          },
-          {
-            x: "2021-12-09 ",
-            y: 50,
-          },
-        ],
+  if (data) {
+    const stackDatas = [...data.getAllStudyStack.node];
+    //学習リストで取得したデータをグラフ用に変換する
+    for (const stackData of stackDatas) {
+      firstDatasets.push({
+        label: stackData.skillTagId,
+        data: [{ x: stackData.createdAt, y: stackData.timeStack }],
+      });
+    }
+    //グラフ用に変換したデータを同じ技術内容ごとに振り分けて技術内容ごとに色分けする
+    for (let i = 0; i < firstDatasets.length; i++) {
+      if (firstDatasets[i]) {
+        for (let j = i + 1; j < firstDatasets.length; j++) {
+          if (firstDatasets[j]) {
+            if (firstDatasets[i].label === firstDatasets[j].label) {
+              firstDatasets[i].data.push(firstDatasets[j].data[0]);
+              delete firstDatasets[j];
+            }
+          }
+        }
+        datasets.push({
+          label: firstDatasets[i].label,
+          data: firstDatasets[i].data,
+          backgroundColor: backgroundColors[i],
+          borderColor: borderColors[i],
+          borderWidth: 1,
+        });
+      }
+    }
+  }
 
-        backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-        borderColor: ["rgba(54, 162, 235, 1)"],
-        borderWidth: 1,
-      },
-      // {
-      //   label: "TypeScript",
-      //   data: [120, 20, 40, 10, 40, 35], //学習時間
-
-      //   backgroundColor: ["rgba(255, 206, 86, 0.2)"],
-      //   borderColor: ["rgba(255, 206, 86, 1)"],
-      //   borderWidth: 1,
-      // },
-      // {
-      //   label: "JavaScript",
-      //   data: [120, 20, 40, 40, 20, 35], //学習時間
-
-      //   backgroundColor: ["rgba(75, 192, 192, 0.2)"],
-      //   borderColor: ["rgba(75, 192, 192, 1)"],
-      //   borderWidth: 1,
-      // },
-      // {
-      //   label: "GraphQL",
-      //   data: [120, 20, 40, 100, 20, 35], //学習時間
-
-      //   backgroundColor: ["rgba(153, 102, 255, 0.2)"],
-      //   borderColor: ["rgba(153, 102, 255, 1)"],
-      //   borderWidth: 1,
-      // },
-      // {
-      //   label: "MongoDB",
-      //   data: [120, 20, 40, 50, 20, 35], //学習時間
-
-      //   backgroundColor: ["rgba(255, 159, 64, 0.2)"],
-      //   borderColor: ["rgba(255, 159, 64, 1)"],
-      //   borderWidth: 1,
-      // },
-    ],
-  };
+  const chartDatas = { datasets };
 
   //型をつけないとoptionを追加できない
   const options: ChartOptions<any> = {
@@ -140,16 +97,16 @@ export const StackTime = memo(() => {
         stacked: true,
         type: "time",
         time: {
-          unit: "week",
+          unit: "day",
           displayFormats: {
             quarter: "MMM YYYY",
           },
         },
 
-        ticks: {
-          min: "2021-10-07 09:00:28",
-          max: "2021-11-07 01:00:28",
-        },
+        // ticks: {
+        //   min: "2021-10-07 09:00:28",
+        //   max: "2021-11-07 01:00:28",
+        // },
       },
 
       yAxes: {
@@ -164,8 +121,8 @@ export const StackTime = memo(() => {
       <Button>月別</Button>
       <Button>日別</Button>
       <Flex>
-        <Box w="1000px">
-          <Bar data={data} options={options} />
+        <Box>
+          <Bar data={chartDatas} options={options} height={400} width={500} />
         </Box>
         <canvas></canvas>
       </Flex>
