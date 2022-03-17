091cd3fab30b5570c4685598c1b37373e6ed7f99
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-18 00:35:06 +0900

    初期値上手く表示されない問題解決

--- client/src/hooks/editMe/useSpecUserInfo.ts
+++ client/src/hooks/editMe/useSpecUserInfo.ts
@@ -1,4 +1,10 @@
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useCookies } from "react-cookie";
@@ -9,7 +15,6 @@ import {
   GetUserByIdDocument,
   useGetSheetByUserIdQuery,
   useUpdateSpecProjectMutation,
-  useUpdateSpecTechInfoMutation,
   useUpdateSpecUserInfoMutation,
 } from "../../types/generated/graphql";
 import { useToast } from "@chakra-ui/react";
@@ -18,36 +23,43 @@ import { useToast } from "@chakra-ui/react";
  * バリデーションチェック.
  */
 const schema = yup.object().shape({
-  //
+  //スタッフID
   stuffID: yup
     .string()
     .trim()
     .required("スタッフIDを入力してください")
     .typeError("スタッフIDを入力してください")
-    .max(5, "スタッフIDは5文字以内で入力してください"),
+    .min(11, "スタッフIDは11文字で入力してください")
+    .max(11, "スタッフIDは11文字で入力してください"),
+  //年齢
   age: yup
     .number()
     .required("年齢を入力して下さい")
     .typeError("年齢を入力して下さい"),
+  //性別
   gender: yup.string(),
+  //最寄駅
   nearestStation: yup
     .string()
     .trim()
     .required("最寄駅を入力して下さい")
     .typeError("最寄駅を入力して下さい")
     .max(50, "最寄駅は50文字以内で入力して下さい"),
+  //最寄線
   nearestLine: yup
     .string()
     .trim()
     .required("最寄路線を入力して下さい")
     .typeError("最寄路線を入力して下さい")
     .max(50, "最寄路線は50文字以内で入力して下さい"),
+  //稼働開始日
   startWorkDate: yup
     .string()
     .trim()
     .required("稼働開始日を入力して下さい")
     .typeError("稼働開始日を入力して下さい")
     .max(50, "稼働開始日は50文字以内で入力して下さい"),
+  //歴
   seExpAmountYear: yup.number(),
   seExpAmountMonth: yup.number(),
   pgExpAmountYear: yup.number(),
@@ -63,7 +75,7 @@ const schema = yup.object().shape({
  * - handleSubmit:データを入力した際のリアルタイム更新
  * - errors:エラー
  * - onSubmit:更新ボタンを押した時のメソッド
- * @params userData - 初期表示用データ
+ * @params useruserData - 初期表示用データ
  */
 export const useSpecUserInfo = (
   setMenuItem: Dispatch<SetStateAction<string>>,
@@ -75,45 +87,63 @@ export const useSpecUserInfo = (
   /**
    * スペックシート基本情報取得.
    */
-  const { data } = useGetSheetByUserIdQuery({
+  const { data: userData } = useGetSheetByUserIdQuery({
     variables: {
       userId: cookies.ForestaID,
     },
   });
 
-  const [stationData, setStdata] = useState(["", ""]);
-  if (data?.user.node.userInfo.nearestStation) {
-    const array = data?.user.node.userInfo.nearestStation.split("(");
-    setStdata([...array]);
-  }
+  //最寄駅デフォルト値作成に使用
+  const stationuserData = userData?.user.node.userInfo.nearestStation.split(
+    "(",
+  ) as Array<string>;
+  //最寄駅
+  const [station, setStation] = useState("");
+  //最寄線
+  const [line, setLine] = useState("");
 
-  const station = stationData?.[0];
-  const line = String(stationData?.[1]).replace(")", "");
+  //IT歴デフォルト値作成に使用
+  const seHis = Number(userData?.user.node.userInfo.seExpAmount);
+  const pgHis = Number(userData?.user.node.userInfo.pgExpAmount);
+  const itHis = Number(userData?.user.node.userInfo.itExpAmount);
 
-  const [seHis, setSeHis] = useState(0);
-  const [pgHis, setPgHis] = useState(0);
-  const [itHis, setItHis] = useState(0);
+  const [seMonth, setSeMonth] = useState(0);
+  const [seYear, setSeYear] = useState(0);
+  const [pgMonth, setPgMonth] = useState(0);
+  const [pgYear, setPgYear] = useState(0);
+  const [itMonth, setItMonth] = useState(0);
+  const [itYear, setItYear] = useState(0);
 
-  if (data?.user.node.userInfo.seExpAmount) {
-    setSeHis(Number(data?.user.node.userInfo.seExpAmount));
-  }
+  useEffect(() => {
+    /**
+     * 最寄駅を最寄路線を切り離す
+     */
+    {
+      stationuserData && setStation(stationuserData[0]);
+      setLine(String(stationuserData?.[1]).replace(")", ""));
+    }
 
-  if (data?.user.node.userInfo.seExpAmount) {
-    setPgHis(Number(data?.user.node.userInfo.pgExpAmount));
-  }
+    /**
+     * IT経験歴をヵ月→年+ヵ月に修正
+     */
+    setSeYear(Math.floor(Number(seHis) / 12));
+    setSeMonth(Number(seHis) % 12);
 
-  if (data?.user.node.userInfo.seExpAmount) {
-    setItHis(Number(data?.user.node.userInfo.itExpAmount));
-  }
+    setPgYear(Math.floor(Number(pgHis) / 12));
+    setPgMonth(Number(pgHis) % 12);
 
-  const seYear = Math.floor(Number(seHis) / 12);
-  const seMonth = Number(seHis) % 12;
-
-  const pgYear = Math.floor(Number(pgHis) / 12);
-  const pgMonth = Number(pgHis) % 12;
-
-  const itYear = Math.floor(Number(itHis) / 12);
-  const itMonth = Number(itHis) % 12;
+    setItYear(Math.floor(Number(itHis) / 12));
+    setItMonth(Number(itHis) % 12);
+  }, [
+    itHis,
+    pgHis,
+    seHis,
+    stationuserData,
+    userData?.user.node.userInfo.itExpAmount,
+    userData?.user.node.userInfo.nearestStation,
+    userData?.user.node.userInfo.pgExpAmount,
+    userData?.user.node.userInfo.seExpAmount,
+  ]);
 
   // バリデーション機能を呼び出し
   const {
@@ -123,34 +153,34 @@ export const useSpecUserInfo = (
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
-    defaultValues: {
-      stuffID: data?.user.node.userInfo.stuffID,
-      age: data?.user.node.userInfo.age,
-      gender: String(data?.user.node.userInfo.gender),
-      nearestStation: station,
-      nearestLine: line,
-      startWorkDate: data?.user.node.userInfo.startWorkDate,
-      seExpAmountYear: seYear,
-      seExpAmountMonth: seMonth,
-      pgExpAmountYear: pgYear,
-      pgExpAmountMonth: pgMonth,
-      itExpAmountYear: itYear,
-      itExpAmountMonth: itMonth,
-    },
+    // defaultValues: {
+    //   stuffsID: userData?.user.node.userInfo.stuffID,
+    //   age: userData?.user.node.userInfo.age,
+    //   gender: String(userData?.user.node.userInfo.gender),
+    //   nearestStation: station,
+    //   nearestLine: line,
+    //   startWorkDate: userData?.user.node.userInfo.startWorkDate,
+    //   seExpAmountYear: seYear,
+    //   seExpAmountMonth: seMonth,
+    //   pgExpAmountYear: pgYear,
+    //   pgExpAmountMonth: pgMonth,
+    //   itExpAmountYear: itYear,
+    //   itExpAmountMonth: itMonth,
+    // },
   });
 
-  // setValue("stuffID", data?.user.node.userInfo.stuffID);
-  // setValue("age", data?.user.node.userInfo.age);
-  // setValue("gender", data?.user.node.userInfo.gender);
-  // setValue("nearestStation", station);
-  // setValue("nearestLine", line);
-  // setValue("startWorkDate", data?.user.node.userInfo.startWorkDate);
-  // setValue("seExpAmountYear", seYear);
-  // setValue("seExpAmountMonth", seMonth);
-  // setValue("pgExpAmountYear", pgYear);
-  // setValue("pgExpAmountMonth", pgMonth);
-  // setValue("itExpAmountYear", itYear);
-  // setValue("itExpAmountMonth", itMonth);
+  setValue("stuffID", userData?.user.node.userInfo.stuffID);
+  setValue("age", userData?.user.node.userInfo.age);
+  setValue("gender", String(userData?.user.node.userInfo.gender));
+  setValue("nearestStation", station);
+  setValue("nearestLine", line);
+  setValue("startWorkDate", userData?.user.node.userInfo.startWorkDate);
+  setValue("seExpAmountYear", seYear);
+  setValue("seExpAmountMonth", seMonth);
+  setValue("pgExpAmountYear", pgYear);
+  setValue("pgExpAmountMonth", pgMonth);
+  setValue("itExpAmountYear", itYear);
+  setValue("itExpAmountMonth", itMonth);
 
   //トーストの使用
   const toast = useToast();
@@ -173,24 +203,24 @@ export const useSpecUserInfo = (
 
   /**
    * 更新ボタンを押した時に呼ばれる
-   * @param data - 入力したデータ
+   * @param userData - 入力したデータ
    */
   const onSubmit = useCallback(
     async (data: any) => {
-      const neareData = `${data.nearestStation}(${data.nearestLine})`;
+      const neareuserData = `${data.nearestStation}(${data.nearestLine})`;
       const se = data.seExpAmountYear * 12 + data.seExpAmountMonth;
       const pg = data.pgExpAmountYear * 12 + data.pgExpAmountMonth;
       const it = data.itExpAmountYear * 12 + data.itExpAmountMonth;
 
       try {
-        await updateSpecInfo({
+        const res = await updateSpecInfo({
           variables: {
             specUserInfo: {
-              specUserInfoId: "",
+              specUserInfoId: String(userData?.user.node.userInfo.id),
               stuffID: data.stuffID,
               age: data.age,
-              gender: data.geder,
-              nearestStation: String(neareData),
+              gender: String(data.gender),
+              nearestStation: String(neareuserData),
               startWorkDate: data.startWorkDate,
               seExpAmount: Number(se),
               pgExpAmount: Number(pg),
@@ -198,17 +228,32 @@ export const useSpecUserInfo = (
             },
           },
         });
-        cancel();
+
+        if (res.data?.updateSpecUserInfo.status === "error") {
+          toast({
+            title: res.data?.updateSpecUserInfo.msg,
+            status: "error",
+            isClosable: true,
+          });
+        } else {
+          cancel();
+          toast({
+            title: "更新しました",
+            status: "success",
+            position: "bottom-left",
+            isClosable: true,
+          });
+        }
+      } catch (error) {
         toast({
-          title: "更新しました",
-          status: "success",
+          title: "失敗しました",
+          position: "bottom-left",
+          status: "error",
           isClosable: true,
         });
-      } catch (error) {
-        console.log(error);
       }
     },
-    [cancel, toast, updateSpecInfo],
+    [cancel, toast, updateSpecInfo, userData?.user.node.userInfo.id],
   );
 
   return {
