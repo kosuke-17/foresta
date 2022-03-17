96b7884752168eb218e13c832ee136cc35482664
yukimaru5447 <w140113@gmail.com>
2022-03-17 11:34:40 +0900

    型をtypeフォルダに集約

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -36,3 +36,55 @@ export type userInfoEditType = {
   jobType: string;
   githubURL: string;
 };
+
+//技術ツリーの型
+export type TechTree = {
+  treeData: any;
+  techTreeData: any;
+  indexOfTreeData: number;
+};
+
+//技術ブランチの型
+export type TechBranch = {
+  treeData: any;
+  techTreeData: any;
+  indexOfTreeData: number;
+  techBranchData: any;
+  indexOfBranchData: number;
+  cheakedLeaf: (
+    treeId: string,
+    branchId: string,
+    leafId: string,
+    isStatus: boolean,
+  ) => void;
+};
+
+//技術リーフの型
+export type TechLeaf = {
+  treeData: any;
+  techLeafData: any;
+  techBranchData: any;
+  indexOfTreeData: number;
+  indexOfBranchData: number;
+  indexOfLeafData: number;
+  cheakedLeaf: (
+    treeId: string,
+    branchId: string,
+    leafId: string,
+    isStatus: boolean,
+  ) => void;
+};
+
+// エンジニアの型
+export type Engineer = {
+  engineerData: {
+    __typename?: "User" | undefined;
+    id: string;
+    name: string;
+    jobType: string;
+    email: string;
+    password: string;
+    githubURL: string;
+  };
+  key: number;
+};
