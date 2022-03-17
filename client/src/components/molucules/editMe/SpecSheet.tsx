2bf879f6c90e462188587318a63b75e4ec3fb62a
hanawtnb <hanawtnb@icloud.com>
2022-03-17 15:20:40 +0900

    更新機能を実装

--- client/src/components/molucules/editMe/SpecSheet.tsx
+++ client/src/components/molucules/editMe/SpecSheet.tsx
@@ -1,11 +1,8 @@
 import { memo, FC, Dispatch, SetStateAction } from "react";
-import { useCookies } from "react-cookie";
-import { Button, Spinner, Flex } from "@chakra-ui/react";
+import { Button, Textarea } from "@chakra-ui/react";
 
-import { SelectInput } from "../../atoms/editMe/SelectInput";
 import { TextInput } from "../../atoms/editMe/TextInput";
 import { useSpecSheet } from "../../../hooks/editMe/useSpecSheet";
-import { useGetPrAndSheetByUserIdQuery } from "../../../types/generated/graphql";
 
 type Props = {
   setMenuItem: Dispatch<SetStateAction<string>>; //menuItemセット用
@@ -17,33 +14,59 @@ type Props = {
  * @remarks 自己PR、業務外、資格、前職
  */
 export const SpecSheet: FC<Props> = memo(({ setMenuItem, onClose }) => {
-  //cookieからID取得
-  const [cookies] = useCookies();
-
-  const { data, loading, error } = useGetPrAndSheetByUserIdQuery({
-    variables: {
-      userId: cookies.ForestaID,
-    },
-  });
-  console.dir("業務外:" + JSON.stringify(data?.other.node.studyOnOwnTime));
-  console.dir("前職:" + JSON.stringify(data?.other.node.prevJobs));
-  console.dir("資格:" + JSON.stringify(data?.other.node.certification));
-  console.dir("PR:" + JSON.stringify(data?.pr.node.selfIntro));
-
-  //読み込み中時の表示
-  if (loading) {
-    return (
-      <Flex justifyContent="center">
-        <Spinner color="green.400" />
-      </Flex>
+  //public部分基本情報編集hooksを使用(引数に渡してあげつつ、使う機能を宣言)
+  const { handleSubmit, register, errors, onSubmit, prevJobs, setPrevJobs } =
+    useSpecSheet(
+      setMenuItem, //メニューアイテムを空にする
+      onClose, //モーダルを閉じるメソッド
     );
-  }
-  //エラー時の表示
-  if (error) {
-    return <Flex justifyContent="center">Error</Flex>;
-  }
+
   return (
     <>
+      <TextInput
+        registers={register("studyOnOwnTime")}
+        errorMessage={errors.studyOnOwnTime?.message}
+        label="業務外"
+        placeholder="業務外の取り組み"
+      />
+
+      {prevJobs?.map((item: any, index: number) => (
+        <div key={index}>
+          <p>{`前職${index + 1}`}</p>
+          <Textarea
+            value={item?.content}
+            id={item.content}
+            placeholder="前職"
+            onChange={(event) => {
+              const arr = [...prevJobs];
+              arr[index] = { content: event.target.value };
+              setPrevJobs(arr);
+            }}
+          >
+            {item?.content}
+          </Textarea>
+        </div>
+      ))}
+      <Button onClick={() => setPrevJobs([...prevJobs, { content: "" }])}>
+        追加
+      </Button>
+      <br />
+
+      <TextInput
+        registers={register("certification")}
+        errorMessage={errors.certification?.message}
+        label="資格"
+        placeholder="資格"
+      />
+
+      <TextInput
+        registers={register("selfIntro")}
+        errorMessage={errors.selfIntro?.message}
+        label="PR"
+        placeholder="PR"
+      />
+
+      <Button onClick={handleSubmit(onSubmit)}>更新</Button>
       <Button type="button" onClick={onClose} _focus={{ boxShadow: "none" }}>
         キャンセル
       </Button>
