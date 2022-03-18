e3c9155729e3bb8efd60e095eb403b3e0899b98b
yukimaru5447 <w140113@gmail.com>
2022-03-19 02:02:25 +0900

    不要なライブラリ・行を削除

--- client/src/container/templates/TechForest.tsx
+++ client/src/container/templates/TechForest.tsx
@@ -2,14 +2,12 @@ import { FC, useState } from "react";
 import { AreaSelectComp } from "../../components/molucules/techForest/AreaSelectComp";
 import { TechTreeComp } from "../../components/organisms/techForest/TechTreeComp";
 import { useGetUserLeafsByIdQuery } from "../../types/generated/graphql";
-import { Box, Center, SimpleGrid, useToast } from "@chakra-ui/react";
 
 export const TechForest: FC = () => {
   // 技術エリアデータの状態管理、(初期値はフロントエンド)
   const [areaId, setAreaId] = useState<string | undefined>(
     "6219afb4d55e2e236b9062b8",
   );
-
   // 特定のユーザーが保有している技術データ
   const { data } = useGetUserLeafsByIdQuery({
     variables: {
