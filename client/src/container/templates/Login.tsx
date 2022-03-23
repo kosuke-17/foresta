ff35a5b3bfb029ed3b7617633d7449009f3dcfad
yukimaru5447 <w140113@gmail.com>
2022-03-23 11:04:09 +0900

    LoginFormに{}をつける

--- client/src/container/templates/Login.tsx
+++ client/src/container/templates/Login.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import LoginForm from "../../components/organisms/auth/LoginForm";
+import { FC } from "react";
+import { LoginForm } from "../../components/organisms/auth/LoginForm";
 
 export const Login: FC = () => {
   return (
