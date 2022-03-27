d53bace385abaa9e8c1361c2ab5b7409bfe4b767
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-03-27 11:59:06 +0900

    storybookフォルダ整理

---
+++ client/src/stories/StackList/ListTable/StudyListTable.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { StudyListTable } from "../../../components/molucules/stackList/StudyListTable";
+
+export default {
+  component: StudyListTable,
+} as ComponentMeta<typeof StudyListTable>;
+
+export const Default: ComponentStoryObj<typeof StudyListTable> = {};
