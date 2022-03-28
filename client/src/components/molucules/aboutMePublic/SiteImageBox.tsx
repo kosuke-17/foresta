d1f5aca7901adc5cbf9f79f656337bba5d26942f
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-28 18:25:41 +0900

    制作物(portfolio)CSS修正

--- client/src/components/molucules/aboutMePublic/SiteImageBox.tsx
+++ client/src/components/molucules/aboutMePublic/SiteImageBox.tsx
@@ -1,28 +1,20 @@
 import { memo, FC, useState, MouseEvent } from "react";
-import { Box, Flex, Spinner } from "@chakra-ui/react";
-import { useCookies } from "react-cookie";
+import { Box, Flex } from "@chakra-ui/react";
 
 import { SiteDetail } from "./SiteDetail";
 import { ModalSet } from "../../molucules/ModalSet";
 import { SiteImage } from "../../atoms/aboutMePublic/SiteImage";
 import { useModal } from "../../../hooks/useModal";
-import { useGetPortfolioByUserIdQuery } from "../../../types/generated/graphql";
 import { PortfolioType } from "../../../types/types";
 import { XCircleFillIcon } from "@primer/octicons-react";
+import { Heading } from "../../atoms/common/Heading";
+
+type Props = { data: Array<PortfolioType> };
 
 /**
  * 制作物一覧画面.
  */
-export const SiteImageBox: FC = memo(() => {
-  //cookieからID取得
-  const [cookies] = useCookies();
-  const { data, loading, error } = useGetPortfolioByUserIdQuery({
-    variables: {
-      userToken: cookies.ForestaID,
-    },
-  });
-  const portfolioData = data?.portfolios.node as Array<PortfolioType>;
-
+export const SiteImageBox: FC<Props> = memo(({ data }) => {
   //モーダル使用のhooks
   const modalStore = useModal();
   const { onOpen, isOpen, onClose } = modalStore;
@@ -38,22 +30,10 @@ export const SiteImageBox: FC = memo(() => {
     onOpen(e);
   };
 
-  //読み込み中時の表示
-  if (loading) {
-    return (
-      <Flex justifyContent="center">
-        <Spinner color="green.400" />
-      </Flex>
-    );
-  }
-  //エラー時の表示
-  if (error) {
-    return <Flex justifyContent="center">Error</Flex>;
-  }
   //制作物の登録が0の時の表示
-  if (portfolioData?.length === 0) {
+  if (data?.length === 0) {
     return (
-      <Flex justifyContent="center">
+      <Flex justifyContent="center" my={15}>
         <XCircleFillIcon size={24} />
         制作物の登録がありません
       </Flex>
@@ -62,43 +42,29 @@ export const SiteImageBox: FC = memo(() => {
 
   return (
     <>
-      <Box backgroundColor="white" pb={10} mb={10}>
-        <Box
-          width="full"
-          textAlign="center"
-          textColor="white"
-          backgroundColor="gray.400"
-          fontWeight="bold"
-          p={3}
-          mt={5}
-          fontSize={20}
-        >
-          制作物一覧
-        </Box>
-
-        <ModalSet
-          isOpen={isOpen}
-          onClose={onClose}
-          modalTitle={portfolioItem?.title}
-          contents={<SiteDetail siteItem={portfolioItem} />}
-          closeBtnName="とじる"
-        />
-        <Flex gap={4} justifyContent="center" wrap="wrap-reverse">
-          {portfolioData &&
-            portfolioData.map((item) => (
-              <div key={item.id}>
-                <Flex
-                  direction="column"
-                  cursor="pointer"
-                  outline="none"
-                  onClick={(e) => openModal(e, item)}
-                >
-                  <SiteImage siteName={item.title} imageUrl={item.img} />
-                </Flex>
-              </div>
-            ))}
-        </Flex>
-      </Box>
+      <Heading text="Portfolio" />
+      <ModalSet
+        isOpen={isOpen}
+        onClose={onClose}
+        modalTitle={portfolioItem?.title}
+        contents={<SiteDetail siteItem={portfolioItem} />}
+        closeBtnName="とじる"
+      />
+      <Flex gap={4} justifyContent="center" wrap="wrap-reverse">
+        {data &&
+          data.map((item) => (
+            <div key={item.id}>
+              <Flex
+                direction="column"
+                cursor="pointer"
+                outline="none"
+                onClick={(e) => openModal(e, item)}
+              >
+                <SiteImage siteName={item.title} imageUrl={item.img} />
+              </Flex>
+            </div>
+          ))}
+      </Flex>
     </>
   );
 });
