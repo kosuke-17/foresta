00a8adb8fe787fd0f6ff366614988eae8a837e9c
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-03-30 14:20:26 +0900

    PDF発行ボタンの引っ越しと全体のバランス調整

--- client/src/container/templates/AboutMe.tsx
+++ client/src/container/templates/AboutMe.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { useLocation } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+
 import { Private } from "../../components/organisms/aboutMe/Private";
 import { Public } from "../../components/organisms/aboutMe/Public";
-
-import { useLocation } from "react-router-dom";
+import { StackTime } from "../../components/organisms/study/StackTime";
 
 interface State {
   engineerId: string;
@@ -18,8 +20,11 @@ export const AboutMe: FC = () => {
 
   return (
     <>
-      <Public engineerId={engineerId} />
-      {!engineerId && <Private />}
+      <Box width="90%" margin="0 auto 50px auto">
+        <Public engineerId={engineerId} />
+        {!engineerId && <Private />}
+        {/* {engineerId ? <StackTime /> : <Private />} */}
+      </Box>
     </>
   );
 };
