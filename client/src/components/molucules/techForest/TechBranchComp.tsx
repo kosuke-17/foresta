2ee11ccc195c47bdba9442035113a91bd319d4a3
yukimaru5447 <w140113@gmail.com>
2022-03-31 09:43:52 +0900

    共通のAccordionContentコンポーネントを使用してtechBranchを再度作成

--- client/src/components/molucules/techForest/TechBranchComp.tsx
+++ client/src/components/molucules/techForest/TechBranchComp.tsx
@@ -1,17 +1,8 @@
 import { FC, memo } from "react";
 import { TechLeafComp } from "../../atoms/techForest/TechLeafComp";
+import { AccordionContent } from "../../molucules/AccordionContent";
 import { TechLeafData } from "../../../types/types";
-import {
-  Box,
-  Text,
-  Flex,
-  HStack,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-} from "@chakra-ui/react";
+import { Flex, HStack } from "@chakra-ui/react";
 
 type Props = {
   treeData: {
@@ -42,41 +33,32 @@ export const TechBranchComp: FC<Props> = memo(
   ({ treeData, indexOfTreeData, techBranchText, indexOfBranchData }) => {
     return (
       <HStack key={indexOfBranchData}>
-        <Accordion defaultIndex={[0]} allowMultiple>
-          <AccordionItem>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                <Text color="gray.600" fontSize="2xl">
-                  {techBranchText}
-                </Text>
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Flex>
-                {treeData?.[indexOfTreeData].branches[indexOfBranchData]
-                  .leafs &&
-                  treeData?.[indexOfTreeData].branches[
-                    indexOfBranchData
-                  ].leafs.map(
-                    (techLeafData: TechLeafData, indexOfLeafData: number) => {
-                      return (
-                        <TechLeafComp
-                          key={indexOfLeafData}
-                          treeData={treeData}
-                          techLeafTextData={techLeafData.name}
-                          techLeafStatus={techLeafData.isStatus}
-                          indexOfTreeData={indexOfTreeData}
-                          indexOfBranchData={indexOfBranchData}
-                          indexOfLeafData={indexOfLeafData}
-                        />
-                      );
-                    },
-                  )}
-              </Flex>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+        <AccordionContent
+          title={techBranchText}
+          content={
+            <Flex>
+              {treeData?.[indexOfTreeData].branches[indexOfBranchData].leafs &&
+                treeData?.[indexOfTreeData].branches[
+                  indexOfBranchData
+                ].leafs.map(
+                  (techLeafData: TechLeafData, indexOfLeafData: number) => {
+                    return (
+                      <TechLeafComp
+                        key={indexOfLeafData}
+                        treeData={treeData}
+                        techLeafTextData={techLeafData.name}
+                        techLeafStatus={techLeafData.isStatus}
+                        indexOfTreeData={indexOfTreeData}
+                        indexOfBranchData={indexOfBranchData}
+                        indexOfLeafData={indexOfLeafData}
+                      />
+                    );
+                  },
+                )}
+            </Flex>
+          }
+          size={"lg"}
+        />
       </HStack>
     );
   },
